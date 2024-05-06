60fc5dc6b532aa2e2d1030ce24bf78be8104524d
tiyo5151 <s1F102400224@iniad.org>
2024-05-07 00:00:07 +0900

    最初

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,74 @@
 import styles from './index.module.css';
+import { useState } from 'react';
 
 const Home = () => {
+  const [samplePos, setsamplePos] = useState(0);
+  // -1 ->石
+  // 0 ->画像なしセル
+  // 1~8 ->数字セル
+  // 9 ->石+はてな
+  // 10 ->石+旗
+  // 11 ->ボムセル
+  const [bombMap, setbombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  // 0 -> 未クリック
+  // 1 ->左クリック
+  // 2 ->はてな
+  // 3 ->旗
+  const [userInputs, setuserInputs] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  console.log(samplePos);
+
+  const random = () => {
+    for (let n = 0; n < 10; n++) {
+      const rx = Math.floor(Math.random() * 9);
+      const ry = Math.floor(Math.random() * 9);
+      console.log(rx, ry);
+    }
+  };
+
+  // random関数の呼び出し
+  random();
+
+  const clickHandler = (x: number, y: number) => {
+    console.log(x, y);
+  };
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.bombMap}>
+        {bombMap.map((row, y) => (
+          <div key={y} className={styles.row}>
+            {row.map((cell, x) => (
+              <div key={x} className={styles.cell} onClick={() => clickHandler(x, y)}>
+                <div
+                  className={styles.sampleStyle}
+                  style={{ backgroundPosition: `${-30 * samplePos}px 0px` }}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setsamplePos((p) => (p + 1) % 14)}>sample</button>
     </div>
   );
 };
